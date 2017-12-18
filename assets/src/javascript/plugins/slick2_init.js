9c32b19e1be80c7ef8a42061bb5da572225dd6ea
kostya <kostya.vasin1@gmail.com>
2017-12-18 15:15:19 +0200

    add media querries for mobile till section CONTACTS
    not finished

--- assets/src/javascript/plugins/slick2_init.js
+++ assets/src/javascript/plugins/slick2_init.js
@@ -1,37 +1,49 @@
-
-
-   $('.multiple-items').slick({
-  infinite: true,
-  slidesToShow: 3,
-  slidesToScroll: 1,
+$('.multiple-items').slick({
+   infinite: true,
+   slidesToShow: 3,
+   slidesToScroll: 1,
    arrows: true,
-   dots: false
+   dots: false,
+   responsive: [
+      {
+         breakpoint: 768,
+         settings: {
+            slidesToShow: 1,
+            arrows: false,
+            autoplay: true,
+            autoplaySpeed: 2000,
+
+         }
+            }
+
+   ]
 });
 
 'use strict';
 
-(function($){
-        $(".form__adressIcon").mouseover(function() {
-       $(".form__adressTextWrap").show();
-    }); 
+(function ($) {
+   $(".form__adressIcon").mouseover(function () {
+      $(".form__adressTextWrap").show();
+   });
 })(jQuery);
 
-(function($){
-        $(".form__telIcon").mouseover(function() {
-       $(".form__telTextWrap").show();
-    }); 
+(function ($) {
+   $(".form__telIcon").mouseover(function () {
+      $(".form__telTextWrap").show();
+   });
 })(jQuery);
 
-(function($){
-	$(window).scroll(function() {
-    if ($(this).scrollTop() >= 50) {       
-        $('#top-button').fadeIn(200);    
-    } else {
-        $('#top-button').fadeOut(200);       }
-});
-$('#top-button').click(function() {      
-    $('body,html').animate({
-        scrollTop : 0                       
-    }, 500);
-});
+(function ($) {
+   $(window).scroll(function () {
+      if ($(this).scrollTop() >= 50) {
+         $('#top-button').fadeIn(200);
+      } else {
+         $('#top-button').fadeOut(200);
+      }
+   });
+   $('#top-button').click(function () {
+      $('body,html').animate({
+         scrollTop: 0
+      }, 500);
+   });
 })(jQuery);
