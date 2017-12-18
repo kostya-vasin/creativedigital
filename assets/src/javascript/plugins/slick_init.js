d46da4465483b2da6c2e7b1034769c2adabcdb89
kostya <kostya.vasin1@gmail.com>
2017-12-18 19:48:14 +0200

    finish add media querries for tablets till works
    need to finish section Team and other

--- assets/src/javascript/plugins/slick_init.js
+++ assets/src/javascript/plugins/slick_init.js
@@ -1,18 +1,25 @@
 //function createSlick(){   
 //function createSlick(){  
 
-	$(".slider").slick({
-  dots: true,
-  infinite: true,
-  speed: 500,
-  fade: true,
-  cssEase: 'linear'
-  });
+$(".slider").slick({
+   dots: true,
+   infinite: true,
+   speed: 500,
+   fade: true,
+   cssEase: 'linear',
+   autoplay: true,
+   autoplaySpeed: 2000,
+
+   responsive: [
+      {
+         breakpoint: 770,
+         settings: {
+            arrows: false,
+
+         }
+            }
+   ]
+});
 
 
 //};
-
-
-
-
-
